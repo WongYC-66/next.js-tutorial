5fabea58d035700120d4f358de9d95b301514f98
WongYC-66 <yengchien@hotmail.com>
2024-09-11 23:45:18 +0800

    solved lint error

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -5,7 +5,6 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation'
-import { pages } from 'next/dist/build/templates/app-page';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
 
